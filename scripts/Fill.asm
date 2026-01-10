3fed92a42cac1d1968aec49f7739a6b1abb5e60e
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2026-01-09 18:03:31 -0800

    asmb update

--- scripts/Fill.asm
+++ scripts/Fill.asm
@@ -1,3 +1,17 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, 
+// the screen should be cleared.
+
+//// Replace this comment with your code.
+
+
+// check if a key is pressed
 // check if a key is pressed
 (CHECK)
     @SCREEN
@@ -38,7 +52,7 @@
     @i
     D=M
     @CHECK
-    D;JLE
+    D;JLT
 
     @16384
     A=D+A
